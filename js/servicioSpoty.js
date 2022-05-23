b0a228edf00a74155656de9bd6e04bf5adce058e
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-05-23 10:01:39 -0500

    se consume servicio spoty

--- js/servicioSpoty.js
+++ js/servicioSpoty.js
@@ -4,7 +4,7 @@
 const URI='https://api.spotify.com/v1/albums/1tGG9ggFTIX8LZt8lH9hqF/tracks?market=US&limit=10&offset=5'
 
 //2. CONFIGURO LA PETICION
-const TOKEN='Bearer BQCmMeIucMhjeOYRFrqbjbnzhrL9NUrFjkrVeY2gWSCrqf2wr8UBXUTuXgHDRfDBwXxCkSpxLdYJv7eFYjQLiFg7k1qpLetyRvCEG4sWWHFNq5S0WwIOx2D3TwcoNDC9ZgshO6h4VsDpVVGrg6o'
+const TOKEN='Bearer BQBX4EOmbwvtyg_GjrXj_hT1PHVqJtnc_jYHwmQi6JQV6_EFQAc6DrzqJuPWWSsOKxmK65jxffGTeC_DRMVWTAUBATnKLv_eUq0GFBt3zxAjs7PFmMDWW4UB2OzFD1aVTOeBxWxNLTtIeke_BAw'
 
 const PETICION={
     method:"GET",
@@ -19,8 +19,42 @@ fetch(URI,PETICION)
 .then(function(respuesta){
     console.log(respuesta.items) //RESPUESTA DE SPOTIFY SERVER
     //recorreindo el arreglo
+    
+    let fila=document.getElementById("fila")
     respuesta.items.forEach(function(cancion){
+        console.log(cancion)
         console.log(cancion.preview_url)
+
+        //TRAVERSING
+        let columna=document.createElement("div")
+        columna.classList.add("col")
+
+        let tarjeta=document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow")
+
+        let audio=document.createElement("audio")
+        audio.classList.add("w-100")
+        audio.setAttribute("controls","controls")
+        audio.src=cancion.preview_url
+
+        let nombre=document.createElement("p")
+        nombre.classList.add("text-center")
+        nombre.textContent=cancion.name
+
+        let foto=document.createElement("img")
+        foto.classList.add("w-100","img-fluid")
+        foto.src="https://firebasestorage.googleapis.com/v0/b/juegolunes.appspot.com/o/fotobso.jpg?alt=media&token=a6c1098b-be8c-40cc-86f3-c1c13f720601"
+
+        //PADRES E HIJOS (reactDOM)
+        tarjeta.appendChild(nombre)
+        tarjeta.appendChild(foto)
+        tarjeta.appendChild(audio)
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
+
+
+
+        
     })
 })
 .catch(function(respuesta){
